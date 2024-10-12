304853d442eeba50c5340c76915b2e01cb189e70
debanhi <debanhi@LAPTOP-GPBJ36EH>
2024-10-11 18:43:28 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calificaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola! introduce calificacion: ");
+
+            //declaramos variable y la leemos
+            int calificacion = Convert.ToInt32(Console.ReadLine());
+
+            if (calificacion >= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("la calificacion es de " + calificacion + ", aprobatoria");
+
+            }
+            else if (calificacion > 50 && calificacion < 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("la calificacion es de " + calificacion + ", presentar regularizacion");
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("la calificacion es de " + calificacion + ", reprobado");
+            }
+             Console.ReadKey();
+        }
+    }
+}
